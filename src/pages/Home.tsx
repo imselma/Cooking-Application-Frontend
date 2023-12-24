ea0102f76a728fdb5c14f795c4c49e29af18dc18
Selma Imširović <selma.imsirovic@stu.ibu.edu.ba>
2023-12-24 23:45:06 +0100

    Creating components, adding routes, using hooks and states

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container d-flex justify-content-center align-items-center'>
+            <div className='col-12 col-md-9 m-5'>
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="card-title"  style={{ color: 'blue' }}>Welcome to Cooking App!</h2>
+                        <p className="card-text"><b>Elevate your home cooking experience with our app. Where every meal becomes a masterpiece, and your kitchen transforms into a culinary haven.</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
